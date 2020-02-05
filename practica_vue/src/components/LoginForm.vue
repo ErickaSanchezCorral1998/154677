a486f544be8b4cb55dc6442878d557cd927d13f3
Hugo Gallegos <hugo.gallegos.g7@gmail.com>
2020-02-05 08:21:15 -0700

    Login
    Login hecho en clase del 4 de febrero

---
+++ practica_vue/src/components/LoginForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <div class="text-left">
+      <h3>We are <b>aroma</b>
+      </h3>
+      <h6>Welcome Back, Please login</h6>
+      <h6>to your account</h6>
+    </div>
+    <div class="col form-group">
+      <input
+        type="email"
+        placeholder="Correo"
+        class="form-control mb-2"
+      >
+      <input
+        type="password"
+        placeholder="Contraseña"
+        class="form-control mb-2"
+      >
+    </div>
+    <div class="form-group m5">
+      <div class="row">
+        <div class="col text-left">
+          <input type="checkbox" />Remember me
+        </div>
+        <div class="col text-right">
+          <small>Forgot password?</small>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="row">
+        <div class="col">
+          <button class="btn btn-purple btn-dark btn-block">Login</button>
+        </div>
+        <div class="col">
+          <button class="btn  btn-light btn-block">Sign up</button>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
+<script lang="js">
+export default {
+  name: 'LoginForm'
+}
+</script>
+<style lang="scss">
+.btn-purple {
+  background: darkviolet;
+  &:hover,
+  &:active {
+    background: rgb(43, 4, 43);
+  }
+}
+</style>
---
+++ practica_vue/src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--Container nos da un ancho maximo de 1140px-->
+  <div class="login container">
+    <div class="card p-3">
+      <h1>Bienvenido</h1>
+      <div class="row">
+        <div class="col">
+          <img
+            src="@/assets/pexels-photo-1034662.jpeg"
+            height="90%"
+          />
+        </div>
+        <div class="col">
+          <LoginForm></LoginForm>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+// @ is an alias to /src
+import LoginForm from '@/components/LoginForm'
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
